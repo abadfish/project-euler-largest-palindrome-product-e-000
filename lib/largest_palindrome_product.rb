57d05eb5de43a3d3c335e422a29c2422e9342260
abadfish <kirsten.ofarrell@gmail.com>
2015-12-04 13:04:34 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+# def palindrome?(n)
+#   n == n.to_s.reverse.to_i
+# end
+
+def largest_palindrome_product
+  threes = 101.upto(999)
+  palindromes = []
+  threes.each do |i|
+    threes.each do |j|
+      product = i * j
+      if product.to_s == product.to_s.reverse
+        palindromes.push(product)
+      end
+    end
+  end
+  palindromes.max
+end
+
+
+
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def answer
+    threes = 101.upto(999)
+    palindromes = []
+    threes.each do |i|
+      threes.each do |j|
+        product = i * j
+        if product.to_s == product.to_s.reverse
+          palindromes.push(product)
+        end
+      end
+    end
+    palindromes.max
+  end
+
+end
